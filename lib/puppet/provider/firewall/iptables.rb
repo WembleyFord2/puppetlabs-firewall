2efbd9512731606cc28cbafd0240ee3799225779
Dave Ford <dave.ford@bristol.ac.uk>
2025-05-15 13:05:12 +0100

    update debug

--- lib/puppet/provider/firewall/iptables.rb
+++ lib/puppet/provider/firewall/iptables.rb
@@ -691,8 +691,6 @@ Puppet::Type.type(:firewall).provide :iptables, :parent => Puppet::Provider::Fir
     end
 
     resource_list.each do |res|
-      Puppet.info("Hello World")
-      Puppet.notice(res)
       resource_value = nil
       if (resource[res]) then
         resource_value = resource[res]
